eb0887861eddc89a9b8b242eff656c6370a3b4fe
JosephJasin <Josef20606@gmail.com>
2021-10-10 22:25:38 +0300

    Https support

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
+const https = require('https');
 const http = require('http');
+const fs = require('fs');
 const express = require('express');
 
 const {Server: SocketIoServer} = require('socket.io');
 
-const {port} = require('config');
+const {httpsPort, httpPort} = require('config');
 const handlers = require('./eventHandlers.js');
 
 const app = express();
 
-const server = http.createServer(app);
-const io = new SocketIoServer(server);
+const options = {
+    cert: fs.readFileSync(rootDir + '/cert/josephjasin_me.crt'),
+    ca: fs.readFileSync(rootDir + '/cert/josephjasin_me.ca-bundle'),
+    key: fs.readFileSync(rootDir + '/cert/server.key'),
+}
+
+const httpsServer = https.createServer(options, app);
+const httpServer = http.createServer(app);
+
+const io = new SocketIoServer(httpsServer);
 
 if (process.env.NODE_ENV === 'development') {
     const livereload = require("livereload");
@@ -27,7 +37,15 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.static('public'));
 
-app.get('/*' , (req, res) => {
+app.enable('trust proxy');
+
+app.use((req, res, next) => {
+    req.secure ? next() :
+        res.redirect('https://' + req.headers.host + req.url)
+});
+
+
+app.get('/*', (req, res) => {
     res.sendFile(rootDir + '/public/index.html');
 })
 
@@ -38,5 +56,6 @@ io.on('connection', socket => {
     handlers.registerMessagesHandler(io, socket);
 });
 
-server.listen(port);
+httpsServer.listen(httpsPort);
+httpServer.listen(httpPort);
 
